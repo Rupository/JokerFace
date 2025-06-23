75b19de43b7f3079379978be2e0178b7c0732561
Rupsha Ray <94976219+Rupository@users.noreply.github.com>
2025-06-23 15:18:44 +0530

    testing out page to page shifts

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-from nicegui import ui
+import api_router_example
+import class_example
+import function_example
+import home_page
+import theme
 
-ui.icon('thumb_up')
-ui.markdown('This is **Markdown**.')
-ui.html('This is <strong>HTML</strong>.')
-with ui.row():
-    ui.label('CSS').style('color: #888; font-weight: bold')
-    ui.label('Tailwind').classes('font-serif')
-    ui.label('Quasar').classes('q-ml-xl')
-ui.link('NiceGUI on GitHub', 'https://github.com/zauberzeug/nicegui')
+from nicegui import app, ui
 
-ui.run(title="JokerFace", favicon='🃏')
+
+# Example 1: use a custom page decorator directly and putting the content creation into a separate function
+@ui.page('/')
+def index_page() -> None:
+    with theme.frame('Homepage'):
+        home_page.content()
+
+
+# Example 2: use a function to move the whole page creation into a separate file
+function_example.create()
+
+# Example 3: use a class to move the whole page creation into a separate file
+class_example.ClassExample()
+
+# Example 4: use APIRouter as described in https://nicegui.io/documentation/page#modularize_with_apirouter
+app.include_router(api_router_example.router)
+
+ui.run(title='Modularization Example')
