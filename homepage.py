4491afaed488d7189d86258d8ff922f753560a45
Rupsha Ray <94976219+Rupository@users.noreply.github.com>
2025-06-24 15:56:34 +0530

    add card assets and basic design page

---
+++ homepage.py
@@ -0,0 +1,32 @@
+from nicegui import ui
+
+ui.markdown('#### **Design Your Joker**')
+with ui.row().classes('items-stretch'):
+    with ui.card():
+        ui.label("Avatar")
+        ui.button('Save')
+
+    with ui.card():
+        ui.label = ("Edit JokerBot Code:")
+        editor = ui.codemirror(
+'''def __init__(self):
+    pass
+    
+def preflop(self, game_state):
+    pass
+
+def postflop(self, game_state):
+    pass
+
+def turn(self, game_state):
+    pass
+
+def river(self, game_state):
+    pass''', 
+
+    language='Python', theme='vscodeLightStyle')
+        #ui.button('Save')
+        editor.classes('h-[40vh]')
+
+ui.run(favicon='🃏', title='Home')
+
---
+++ login.py
@@ -0,0 +1,8 @@
+# To be filled in later.
+
+# We need a login page for users to make accounts so that they can 
+# save their bot code and don't have to rewrite it every time
+
+# Accounts will also help with bot vs bot matches. Actually... we may not even need live netplay
+# If the bots get saved, any user can fight against another user's bots. Both users don't need to
+# be online for the 1v1 matches
